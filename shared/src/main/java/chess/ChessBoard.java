0b913d0c7d4ed98fac2c03d823d88a19dbc2b8b3
Jonathan Smith <jpricesmith2016@gmail.com>
2026-06-30 15:36:12 -0600

    Project 1: Making some changes to fix errors in testing : Implementing Clonable.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessBoard {
+public class ChessBoard implements Cloneable {
 
     ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
