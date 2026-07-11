3b5ed1d216e61fdba4909b4569d7a63a3fc51c05
Jonathan Smith <jpricesmith2016@gmail.com>
2026-07-11 15:31:32 -0500

    Project 3: Moving Web because autograder couldn't find it

--- shared/src/test/java/passoff/chess/ChessPieceTests.java
+++ shared/src/test/java/passoff/chess/ChessPieceTests.java
@@ -42,9 +42,9 @@ public class ChessPieceTests extends EqualsTestingUtility<ChessPiece> {
         // 6 piece types * 2 team colors = 12 different pieces
         Collection<ChessPiece> allPossiblePieces =
                 Arrays.stream(ChessPiece.PieceType.values())
-                .flatMap(pieceType -> Arrays.stream(ChessGame.TeamColor.values())
-                .map(teamColor -> new ChessPiece(teamColor, pieceType)))
-                .toList();
+                        .flatMap(pieceType -> Arrays.stream(ChessGame.TeamColor.values())
+                                .map(teamColor -> new ChessPiece(teamColor, pieceType)))
+                        .toList();
 
         // 8 rows * 8 cols * 12 pieces = 768 evaluations - 32 pawns on back rows
         for (int i = 1; i <= 8; i++) {
@@ -58,9 +58,9 @@ public class ChessPieceTests extends EqualsTestingUtility<ChessPiece> {
 
                     board.addPiece(position, piece);
                     Assertions.assertDoesNotThrow(
-                            () -> ChessPiece.pieceMoves(board, position),
+                            () -> piece.pieceMoves(board, position),
                             "No pieces anywhere on the board should throw an error. "
-                            + "Tested: " + piece + " at " + position + ".");
+                                    + "Tested: " + piece + " at " + position + ".");
                 }
 
                 board.addPiece(position, null);
