159d2d6c438d14e894db2e2b7f27f13dd53f0922
Jonathan Smith <jpricesmith2016@gmail.com>
2026-07-18 19:00:54 -0600

    Project 4: Remove Commented Out Code

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -6,10 +6,6 @@ import model.UserData;
 public interface UserDAO {
     void createUser(UserData u) throws DataAccessException;
 
-//    void updateUser(UserData user);
-
-//    void deleteUser(String username);
-
     void clearUser() throws DataAccessException;
 
     boolean containsUser(String username) throws DataAccessException;
