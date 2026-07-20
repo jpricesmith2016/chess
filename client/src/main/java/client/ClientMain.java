5dcb7033ed28a6a75e8aa7a11ffb6b959070390b
Jonathan Smith <jpsmith1@byu.edu>
2026-07-20 14:05:58 -0600

    Project 5: Preparing structure

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -3,8 +3,24 @@ package client;
 import chess.*;
 
 public class ClientMain {
+
+    private static String username;
+    private static String authToken;
+
     public static void main(String[] args) {
+        String serverUrl = "http://localhost:8080";
+
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        try {
+            new ChessClient(serverUrl).run();
+        } catch (Throwable e) {
+            System.out.printf("Unable to start server: %s%n", e.getMessage());
+        }
     }
 }
