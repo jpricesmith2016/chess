f62cd34f4fd2ad02d4907c4e9d1f99c2797c85bb
Jonathan Smith <jpsmith1@byu.edu>
2026-07-22 11:15:00 -0600

    Project 5: Built out Chess Board Output

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -2,24 +2,16 @@ package client;
 
 import java.util.Arrays;
 
+import model.GameData;
 import ui.EscapeSequences;
-import static ui.EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY;
+import static ui.EscapeSequences.*;
 
 public class ChessClient {
 
     private static ServerFacade httpClient;
-    public static int gameID;
-    public static String username;
-    public static boolean whiteTeam;
 
     public ChessClient(ServerFacade httpClient) {
         ChessClient.httpClient = httpClient;
-    }
-
-    public void setGameInfo(int gameID, String user, Boolean whiteTeam) {
-        ChessClient.gameID = gameID;
-        ChessClient.username = user;
-        ChessClient.whiteTeam = whiteTeam;
     }
 
     private final String helpString =
@@ -30,43 +22,6 @@ public class ChessClient {
             """;
 
     void printPrompt() {
-        System.out.print(EscapeSequences.ERASE_SCREEN);
-        System.out.print(SET_TEXT_COLOR_LIGHT_GREY + "\n[Chess_Game] >>> ");
-
-        if (whiteTeam) {
-
-            System.out.println("  a b c d e f g h");
-
-            for (int row = 7; row >= 0; row--) {
-                System.out.print((row + 1) + " ");
-
-                for (int col = 0; col < 8; col++) {
-                    drawSquare(getBoard[row][col], row, col);
-                }
-
-                System.out.println(" " + (row + 1));
-            }
-
-            System.out.println("  a b c d e f g h");
-
-        } else {
-
-            System.out.println("  h g f e d c b a");
-
-            for (int row = 0; row < 8; row++) {
-                System.out.print((row + 1) + " ");
-
-                for (int col = 7; col >= 0; col--) {
-                    drawSquare(getBoard[row][col], row, col);
-                }
-
-                System.out.println(" " + (row + 1));
-            }
-
-            System.out.println("  h g f e d c b a");
-        }
-
-
         System.out.print(SET_TEXT_COLOR_LIGHT_GREY + "\n[Chess_Game] >>> ");
     }
 
@@ -83,20 +38,5 @@ public class ChessClient {
         } catch (Exception e) {
             return e.getMessage();
         }
-    }
-
-    private static int[][] transpose(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        // Create a new matrix with swapped dimensions
-        int[][] result = new int[cols][rows];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                result[j][i] = matrix[i][j]; // Swap indices
-            }
-        }
-        return result;
     }
 }
--- client/src/main/java/client/GameClient.java
+++ client/src/main/java/client/GameClient.java
@@ -1,26 +1,122 @@
 package client;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import model.GameData;
+import ui.EscapeSequences;
+
 import java.util.Arrays;
 
-import static ui.EscapeSequences.SET_TEXT_COLOR_LIGHT_GREY;
+import static ui.EscapeSequences.*;
+import static ui.EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+import static ui.EscapeSequences.SET_BG_COLOR_GREEN;
 
 public class GameClient {
 
     private static ServerFacade httpClient;
+    public static String username;
+    public static boolean whiteTeam;
+    private static GameData game;
+    private static ChessBoard board;
 
     public GameClient(ServerFacade httpClient) {
         GameClient.httpClient = httpClient;
     }
 
+    public void setGameInfo(GameData game, String user, Boolean whiteTeam) {
+        GameClient.game = game;
+        GameClient.username = user;
+        GameClient.whiteTeam = whiteTeam;
+    }
+
     private final String helpString =
             """
             Options:
-            Exit the game: "q", "quit"
+            Exit the program: "q", "quit"
             Print this message: "h", "help"
             """;
 
     void printPrompt() {
-        System.out.print(SET_TEXT_COLOR_LIGHT_GREY + "\n[LOGGED_IN] >>> ");
+        System.out.print(EscapeSequences.ERASE_SCREEN);
+        System.out.println(SET_TEXT_COLOR_LIGHT_GREY + "\n[Chess_Game] >>> Game_ID: " + game.gameID());
+
+        printBoard();
+
+        System.out.print(SET_TEXT_COLOR_LIGHT_GREY + "\n[Chess_Game] >>> " + RESET_TEXT_COLOR);
+    }
+
+    void printBoard() {
+        String squareColor = SET_BG_COLOR_GREEN;
+        ChessBoard newBoard = game.game().getBoard();
+
+        if (whiteTeam) {
+
+            System.out.println(SET_BG_COLOR_LIGHT_GREY + " abcdefgh ");
+
+            for (int row = 7; row >= 0; row--) {
+                System.out.print((row + 1) + " ");
+
+                for (int col = 0; col < 8; col++) {
+                    squareColor = (squareColor.equals(SET_BG_COLOR_GREEN)) ? SET_BG_COLOR_DARK_GREEN : SET_BG_COLOR_GREEN;
+                    ChessPosition pos = new ChessPosition(row, col);
+                    System.out.print((newBoard.getPiece(pos) != board.getPiece(pos) ? SET_BG_COLOR_MAGENTA : squareColor)
+                            + getSquare(pos));
+                }
+
+                System.out.println(SET_BG_COLOR_LIGHT_GREY + (row + 1));
+            }
+
+            System.out.println(SET_BG_COLOR_LIGHT_GREY + " abcdefgh " + RESET_BG_COLOR);
+
+        } else {
+
+            System.out.println(SET_BG_COLOR_LIGHT_GREY + " hgfedcba ");
+
+            for (int row = 0; row < 8; row++) {
+                System.out.print((row + 1) + " ");
+
+                for (int col = 7; col >= 0; col--) {
+                    squareColor = (squareColor.equals(SET_BG_COLOR_GREEN)) ? SET_BG_COLOR_DARK_GREEN : SET_BG_COLOR_GREEN;
+                    ChessPosition pos = new ChessPosition(row, col);
+                    System.out.print((newBoard.getPiece(pos) != board.getPiece(pos) ? SET_BG_COLOR_MAGENTA : squareColor)
+                            + getSquare(pos));
+                }
+
+                System.out.println(SET_BG_COLOR_LIGHT_GREY + (row + 1));
+            }
+
+            System.out.println(SET_BG_COLOR_LIGHT_GREY + " hgfedcba " + RESET_BG_COLOR);
+        }
+
+        board = newBoard;
+    }
+
+    private String getSquare(ChessPosition pos) {
+        ChessPiece piece = board.getPiece(pos);
+
+        switch (piece.getPieceType()) {
+            case KING -> {
+                return (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? WHITE_KING : BLACK_KING;
+            }
+            case QUEEN -> {
+                return (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? WHITE_QUEEN : BLACK_QUEEN;
+            }
+            case BISHOP -> {
+                return (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? WHITE_BISHOP : BLACK_BISHOP;
+            }
+            case KNIGHT -> {
+                return (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? WHITE_KNIGHT : BLACK_KNIGHT;
+            }
+            case ROOK -> {
+                return (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? WHITE_ROOK : BLACK_ROOK;
+            }
+            case PAWN -> {
+                return (piece.getTeamColor() == ChessGame.TeamColor.WHITE) ? WHITE_PAWN : BLACK_PAWN;
+            }
+        }
+        return " ";
     }
 
     String eval(String input) {
