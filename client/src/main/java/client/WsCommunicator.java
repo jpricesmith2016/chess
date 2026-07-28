dd00abe19bb8edde9c36179139396d91970d0a90
Jonathan Smith <jpsmith1@byu.edu>
2026-07-28 17:27:24 -0600

    Project 6: Started working on client code

--- client/src/main/java/client/WsCommunicator.java
+++ client/src/main/java/client/WsCommunicator.java
@@ -1,6 +1,20 @@
 package client;
 
-public class WsCommunicator {
+import com.google.gson.Gson;
+import websocket.messages.*;
+import websocket.commands.*;
+
+import jakarta.websocket.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WsCommunicator extends Endpoint{
+
+    Session session;
+
+
     public WsCommunicator(String serverURL) {
     }
 }
